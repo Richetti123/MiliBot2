a64eec5a11a972f5f8b46ffa32e4f800c8cb505e
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2025-10-16 03:52:20 +0200

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, text, participants, args, command }) => {
   if (command == 'tagall' || command == 'invocar' || command == 'todos' || command == 'invocación' || command == 'invocacion') {
     let mensaje = args.join` `;
-    let encabezado = `@richetti_123 ${mensaje}`;
-    let texto = `${global.packname} te invoca despiértate 😡!!\n\n${encabezado}\n\n`;
+    let encabezado = `@chemili.ff ${mensaje}`;
+    let texto = `𓆩♡𓆪 CHE MILI LOS INVOCA APAREZCAN!!! 😡\n\n${encabezado}\n\n`;
 
     for (let mem of participants) {
-      texto += `😼 @${mem.id.split('@')[0]}\n`;
+      texto += `👤 @${mem.id.split('@')[0]}\n`;
     }
     texto += `${global.packname}`;
     conn.sendMessage(m.chat, { text: texto, mentions: participants.map(a => a.id) });
